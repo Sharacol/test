41663c6cfe31018ad7794faf4b1ecce867167a5c
Sharacol <semarcellino@gmail.com>
2019-12-30 21:58:51 +0000

    adding date logic

--- Create account/src/Components/Backend/Purchases/index.js
+++ Create account/src/Components/Backend/Purchases/index.js
@@ -4,6 +4,8 @@ import { withFirebase } from '../../User Code/Firebase'
 import { withAuthentication } from '../../User Code/Session'
 import { Form, InputGroup } from 'react-bootstrap'
 
+import { lastDayOfWeek, isSunday, subDays, getDate, getYear, getMonth} from 'date-fns'
+
 class PurchaseForm extends Component {
 
     constructor(props) {
@@ -48,35 +50,57 @@ class PurchaseForm extends Component {
         console.log("GOOD")
     }
 
+    getPurchaseRanges = () =>{
+        const currentDate = new Date();
+        let nextSaturday = lastDayOfWeek(currentDate)
+        let isItSunday = false;
+        let pastSunday = currentDate;
+        while(!isSunday(pastSunday)){
+            pastSunday = subDays(pastSunday,1);
+        }
+        let dateRange = 
+            `${getDate(pastSunday)}\\${getYear(pastSunday)}-`+
+            `${getDate(nextSaturday)}\\${getYear(nextSaturday)}`
+        let monthRange = 
+            `${getMonth(pastSunday) + 1}\\${getYear(pastSunday)}-`+
+            `${getMonth(nextSaturday) + 1}\\${getYear(nextSaturday)}`
+
+        return ({
+            dateRange,
+            monthRange
+        })
+    }
+
+
     render() {
         const { test, purchases, categories, necessities } = this.state;
-
+        console.log(this.getPurchaseRanges().monthRange)
         return (
             <Form onSubmit={this.onSubmit}>
-                <Form.Group controlId="exampleForm.ControlInput1">
+                <Form.Group controlId="PurchaseTitle">
                     <Form.Label>Title</Form.Label>
                     <Form.Control name="Title" onChange={this.onChange} />
                 </Form.Group>
-                <Form.Group controlId="exampleForm.ControlInput1">
+                <Form.Group controlId="PurchasePrice">
                     <InputGroup.Prepend>
                         <InputGroup.Text id="inputGroupPrepend">$</InputGroup.Text>
                     </InputGroup.Prepend>
                     <Form.Label>Price</Form.Label>
                     <Form.Control name="Price" onChange={this.onChange} aria-describedby="inputGroupPrepend"/>
                 </Form.Group>
-                <Form.Group controlId="exampleForm.ControlSelect2">
+                <Form.Group controlId="NecessityChoice">
                     <Form.Label>Necessity</Form.Label>
                     <Form.Control as="select" name="Necessity" onChange={this.onChange}>
                         {
                             necessities.map((val, index) => {
                                 return (
-                                    <option key={index}>{val}</option>
+                                    <option id = {index} key={index}>{val}</option>
                                 )
                             })
                         }
                     </Form.Control>
                 </Form.Group>
-                <Form.Group controlId="exampleForm.ControlSelect2" >
+                <Form.Group controlId="CategoryChoice" >
                     <Form.Label>Category</Form.Label>
                     <Form.Control as="select" name="Category" onChange={this.onChange}>
                         {
